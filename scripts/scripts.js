2388c5c2c400ae8ec1709d01610557449bafcbc4
frontam <chilakiles53@gmail.com>
2025-11-17 17:37:06 -0600

    add code in javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,72 @@
 let editProfile = document.getElementById("edit-boton");
 let closeForm = document.getElementById("cross");
-let form = document.getElementById("editform");
+let EditForm = document.getElementById("editform");
 
 function actForm() {
-  form.classList.remove("popup");
+  EditForm.classList.remove("popup");
 }
 
 editProfile.addEventListener("click", actForm);
 
 function desForm() {
-  form.classList.add("popup");
+  EditForm.classList.add("popup");
 }
 closeForm.addEventListener("click", desForm);
 
-let borrar = document.querySelector("content-profile");
+let TextName = document.getElementById("profile-name").innerText;
+let TextHobbie = document.getElementById("hobbie-profile").innerText;
 
-let blanca = document.getElementById("profile-name").innerText;
-let liz = document.getElementById("hobbie-profile").innerText;
+const formName = document.getElementById("form-name");
+const formHobbie = document.getElementById("form-hobbie");
 
-let formName = document.getElementById("form-name");
-let formHobbie = document.getElementById("form-hobbie");
+formName.value = TextName;
+formHobbie.value = TextHobbie;
 
-formName.value = blanca;
-formHobbie.value = liz;
+const form = document.getElementById("formulario");
+const buttonSave = document.getElementById("form-buttonsave");
+
+const inputs = form.querySelectorAll("input, textarea");
+
+function formulario() {
+  let completo = true;
+
+  inputs.forEach((input) => {
+    if (input.value.trim() === "") {
+      completo = false;
+    }
+  });
+
+  if (completo) {
+    buttonSave.classList.add("popup__container__bottomsave");
+  } else {
+    buttonSave.classList.remove("popup__container__bottomsave");
+  }
+}
+
+form.addEventListener("input", formulario);
+formulario();
+
+let formElement = document.querySelector("#formulario");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = formElement.querySelector(".popup__container-profile");
+  let jobInput = formElement.querySelector(".popup__container-hobbie");
+
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+
+  let profileName = document.getElementById("profile-name");
+  let profileJob = document.getElementById("hobbie-profile");
+
+  profileName.textContent = nameValue;
+  profileJob.textContent = jobValue;
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+function closeBotton() {
+  EditForm.classList.add("popup");
+}
+buttonSave.addEventListener("click", closeBotton);
